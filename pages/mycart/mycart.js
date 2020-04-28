5f83aec660753f7fa282fc97ba0db81e9851b816
longhuaiyang <545482979@qq.com>
2020-04-28 17:40:24 +0800

    购物车
    购物车添加删除商品

--- pages/mycart/mycart.js
+++ pages/mycart/mycart.js
@@ -1,6 +1,10 @@
 //Page Object
+var httpUtil = require('../../utils/httpUtils');   //相对路径
+
 Page({
   data: {
+    productsArray: [],
+
     cartAllIn: false,
     cart: [],
     cartTotal: 0,
@@ -8,7 +12,84 @@ Page({
   },
   //options(Object)
   onLoad: function (options) {
+    let params = {
+      session_key: wx.getStorageSync("session_key"),
+    };
+    var that = this;
+    httpUtil.postRequest(getApp().data.serverUrl + "/myCart/getShopCarList", params, function (res) {
+      console.log("res：", res);
+      that.setData({
+        productsArray: res
+      })
+    }, function (err) {
+      console.log("err：", err);
+      wx.showToast({
+        title: '获取数据失败!',
+      })
+    });
 
+  },
+
+  onAddProductToMyCart: function (e) {
+    console.log(e);
+    var $data = e.currentTarget.dataset;
+
+    let params = {
+      session_key: wx.getStorageSync("session_key"),
+      goods_id: $data.id
+    };
+    var that = this;
+    httpUtil.postRequest(getApp().data.serverUrl + "/myCart/add", params, function (res) {
+      console.log("res：", res);
+      if (res.code >= 2000 & res.code < 3000) {
+        that.setData(
+          {
+            productsArray: res.data
+          }
+        )
+      } else {
+        wx.showToast({
+          title: res.msg,
+        })
+      }
+    }, function (err) {
+      console.log("err：", err);
+      wx.showToast({
+        title: '失败!',
+      })
+    });
+  },
+
+  onDeleteProductFromMyCart: function (e) {
+    console.log(e);
+    var $data = e.currentTarget.dataset;
+
+    let params = {
+      session_key: wx.getStorageSync("session_key"),
+      goods_id: $data.id
+    };
+    var that = this;
+
+    //删除购物车商品
+    httpUtil.postRequest(getApp().data.serverUrl + "/myCart/delete", params, function (res) {
+      console.log("res：", res);
+      if (res.code >= 2000 & res.code < 3000) {
+        that.setData(
+          {
+            productsArray: res.data
+          }
+        )
+      } else {
+        wx.showToast({
+          title: res.msg,
+        })
+      }
+    }, function (err) {
+      console.log("err：", err);
+      wx.showToast({
+        title: '删除失败!',
+      })
+    });
   },
 
   // 购物车全选操作
