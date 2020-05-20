fc9a10c97a40d8ad1acf6e36d0e025191595e400
longhuaiyang <545482979@qq.com>
2020-05-20 16:43:24 +0800

    完成提货

--- pages/qrCodeReceiveList/qrCodeReceiveList.js
+++ pages/qrCodeReceiveList/qrCodeReceiveList.js
@@ -36,4 +36,41 @@ Page({
   onShow: function () {
 
   },
+  onFinishOrder: function (e) {
+    let list = this.data.list;
+    let orderIdsList = [];
+    //获取订单列表
+    for (let i = 0; i < list.length; i++) {
+      orderIdsList.push(list[i].orderId);
+    }
+
+    //
+    let params = {
+      token: wx.getStorageSync("token"),
+      receiverToken: wx.getStorageSync("receiverToken"),
+      orderIdsList: orderIdsList
+    };
+    var that = this;
+    httpUtil.postRequest(getApp().data.serverUrl + "/finishQrCodeReceiveList", params, function (res) {
+      console.log("res：", res);
+      if (res.code >= 2000 & res.code < 3000) {
+        that.setData({
+          list: []
+        })
+        wx.showToast({
+          title: "成功",
+        })
+      } else {
+        wx.showToast({
+          title: res.msg,
+        })
+      }
+    }, function (err) {
+      console.log("err：", err);
+      wx.showToast({
+        title: '失败!',
+      })
+    });
+  },
+
 });
